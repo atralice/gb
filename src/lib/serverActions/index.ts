cb6d154d1d6f82214af897d941b4181c30c43723
Toni Tralice <atralice@gmail.com>
2026-01-31 21:42:55 -0300

    feat: add index exports for serverActions

---
+++ src/lib/serverActions/index.ts
@@ -0,0 +1,7 @@
+export { serverActionError } from "./serverActionError";
+export type {
+  ServerActionError,
+  ServerActionFlattenedError,
+} from "./serverActionError";
+export { isServerActionError } from "./isServerActionError";
+export { isServerActionSuccess } from "./isServerActionSuccess";
